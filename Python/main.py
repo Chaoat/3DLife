d4043e6c4c4551c7d83b8721717310df80d6e458
Cowinatub <jstevadore@gmail.com>
2018-05-27 12:29:51 +1000

    Stuff

--- Python/main.py
+++ Python/main.py
@@ -12,7 +12,7 @@ def GliderTest():
     TestRule.addRule([2], stayFunction)
     TestRule.addRule([3], birthFunction)
 
-    TestMap = map.Map([20, 10], [False, False], 1)
+    TestMap = map.Map([40, 40], [True, True], 0)
     TestMap[2][1] = 1
     TestMap[3][2] = 1
     TestMap[4][2] = 1
@@ -25,22 +25,29 @@ def GliderTest():
         TestMap.print2D()
 
 def ThreeDTest():
-    TestMap = map.Map([5, 5, 5], [True, True, True], 0)
-    TestMap[1][2][3] = 1
-    TestMap[2][2][3] = 1
-    TestMap[3][2][3] = 1
+    TestMap = map.Map([10, 10, 10], [True, True, True], 0)
+    TestMap[2][1][4] = 1
+    TestMap[3][2][4] = 1
+    TestMap[4][2][4] = 1
+    TestMap[2][3][4] = 1
+    TestMap[3][3][4] = 1
+    TestMap[2][1][5] = 1
+    TestMap[3][2][5] = 1
+    TestMap[4][2][5] = 1
+    TestMap[2][3][5] = 1
+    TestMap[3][3][5] = 1
     TestMap.print3D()
 
     def dieFunction(state):
         return 0
     def stayFunction(state):
         return state
-    def birthFunction(state):
+    def birthStayFunction(state):
         return 1
     TestRule = rule.Rule(True, 3, 2, [1, 1, 1], False, dieFunction)
-    TestRule.addRule([2], stayFunction)
-    TestRule.addRule([3], birthFunction)
-    TestRule.addRule([4], birthFunction)
+    TestRule.addRule([5], stayFunction)
+    TestRule.addRule([6], birthStayFunction)
+    TestRule.addRule([7], stayFunction)
 
     while True:
         input('')
