82ce2a869d05df0b8e507d0544a34249f53198bc
Cowinatub <jstevadore@gmail.com>
2018-06-05 15:47:04 +1000

    wewewewewe

--- run.bat
+++ run.bat
@@ -1,8 +1,7 @@
 REM You have to put the Irrlicht.dll into the same folder as the exe. 
-COMPILER=g++
+set COMPILER=g++
 set BIN_NAME="life.exe"
 set LINKDIRS="" 
 set INCDIRS="-I/path/to/irrlicht/include/ -I/path/to/Python/Python36/include/ -I/path/to/boost/" 
 
-cd C
-make CC="%COMPILER%" BIN_NAME="%BIN_NAME%" LINKDIRS="%LINKDIRS%" INCDIRS="%INCDIRS%" && cmd \K "%BIN_NAME%"
+mingw32-make CC="%COMPILER%" BIN_NAME="%BIN_NAME%" LINKDIRS="%LINKDIRS%" INCDIRS="%INCDIRS%" && cmd \K "%BIN_NAME%"
