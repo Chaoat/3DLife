d15ab95cde69d39bf7383c4a9d0a6b2c4d6e7810
Andrew Persic <aper0008@student.monash.edu>
2018-06-13 21:49:16 +1000

    a diagonal line

--- Python/main.py
+++ Python/main.py
@@ -183,6 +183,10 @@ def DataTransferTest():
         TestMap = fileSystem.loadMapRelative('axis2dtest')
         TestRule = fileSystem.loadRuleRelative("wireworld")
         TestTime = Time(TestMap, TestRule, 10, 1)
+    elif rule == "9":
+        TestMap = fileSystem.loadMapRelative('diagonal2d')
+        TestRule = fileSystem.loadRuleRelative("wireworld")
+        TestTime = Time(TestMap, TestRule, 10, 1)
     TestTime.run()
     # for i in range(20):
     #     TestTime.step()
